6cbd716792a1a737aa7a2508b8622739812911cc
Nick Jackson <nijackson@lincoln.ac.uk>
2012-10-17 09:49:48 +0100

    Added GA tracking code.

--- src/application/views/inc/head.php
+++ src/application/views/inc/head.php
@@ -21,6 +21,20 @@
 		<link rel="stylesheet" href="<?php echo $_SERVER['CWD_BASE_URI']; ?>/ie.min.css">
 		<script src="<?php echo $_SERVER['CWD_BASE_URI']; ?>/html5shiv.min.js"></script>
 	<![endif]-->
+	
+	<script type="text/javascript">
+	
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-27617986-4']);
+		_gaq.push(['_trackPageview']);
+		
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	
+	</script>
  
 </head>
 
