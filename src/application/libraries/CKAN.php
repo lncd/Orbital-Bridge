5577ea0dedb79dbd68539e71c9b4d24b07573492
Harry Newton <h-newton@hotmail.com>
2012-11-08 11:00:07 +0000

    Bridge can edit CKAN Permissons

--- src/application/libraries/CKAN.php
+++ src/application/libraries/CKAN.php
@@ -39,16 +39,15 @@ class CKAN {
 	 *@return ARRAY
 	 */
 
-	public function create_dataset($input_fields, $API_key)
+	public function create_dataset($dataset, $API_key)
 	{
 		//set POST variables
 		$url = 'https://ckan.lincoln.ac.uk/api/rest/dataset';
 
-
 		$fields = array(
-			'title' => $input_fields->title,
-			'name' => $input_fields->uri_slug,
-			'author' => $input_fields->author
+			'title' => $dataset->get_title(),
+			'name' => $dataset->get_uri_slug(),
+			'author' => $dataset->get_author()
 		);
 
 		$fields = json_encode($fields);
@@ -82,16 +81,54 @@ class CKAN {
 		curl_close($ch);
 		$data = json_decode($data);
 		
-		var_dump($data);
 		//Build bridge-object
-		$bridge->title = $data->title;
-		$bridge->uri_slug = url_title($data->title, '_', TRUE);
-		$bridge->creator = $data->author;
-		$bridge->subjects = array(); //JACS CODES
-		$bridge->date = $data->metadata_created;
-		$bridge->type = 'dataset';
-
 		
-		return $bridge;
+		$this->_ci->load->model('Dataset_Object');
+		$dataset = new Dataset_Object();
+		
+		$dataset->set_title($data->title);
+		$dataset->set_uri_slug(url_title($data->title, '_', TRUE));
+		$dataset->set_creator($data->author);
+		$dataset->set_subjects(array()); //JACS CODES
+		$dataset->set_date(strtotime($data->metadata_created));
+		foreach($data->tags as $tag)
+		{
+			$dataset->add_keyword($tag);
+		}
+		$dataset->set_uri_slug($data->url);
+		
+		return $dataset;
+	}
+	
+	
+	public function update_permissions($user, $dataset, $role, $API_key)
+	{
+		//set POST variables
+		$url = 'https://ckan.lincoln.ac.uk/api/action/user_role_update';//{"user": "dread", "domain_object": "freshwateratlasrivers", "roles": ["admin"]}' -H "Authorization:{your-api-key}"
+
+		$fields = array(
+			'user' => $user,
+			'domain_object' => $dataset,
+			'roles' => $role
+		);
+
+		$fields = json_encode($fields);
+
+
+		//open connection
+		$ch = curl_init();
+
+		//set the url, number of POST vars, POST data
+		curl_setopt($ch,CURLOPT_URL, $url);
+		curl_setopt($ch,CURLOPT_POST, count($fields));
+		curl_setopt($ch,CURLOPT_POSTFIELDS, $fields);
+		//curl_setopt($ch, CURLOPT_USERPWD, 'username' . ":" . 'password');
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: ' . $API_key));
+
+		//execute post
+		$result = curl_exec($ch);
+
+		//close connection
+		curl_close($ch);
 	}
 }
---
+++ src/application/models/Dataset_Object.php
@@ -0,0 +1,97 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Bridge_Object Library
+ *
+ * Manages the Bridge Object.
+ *
+ * @category   Library
+ * @package    Orbital
+ * @subpackage Manager
+ * @autho      Harry Newton <hnewton@lincoln.ac.uk>
+ * @link       https://github.com/lncd/Orbital-Manager
+ *
+ * @todo Rewrite to use exceptions.
+ */
+
+class Dataset_Object {
+
+	protected $_title;	
+	protected $_uri_slug;
+	protected $_creators;
+	protected $_subjects; //JACS CODES
+	protected $_date;
+	protected $_keywords = array();
+	
+	//Get and SET $title
+	public function set_title($title)
+	{
+		$this->_title = $title;
+		return TRUE;
+	}
+	
+	public function get_title()
+	{
+		return $this->_title;
+	}
+	
+	//Get and SET $uri_slug
+	public function set_uri_slug($uri_slug)
+	{
+		$this->_uri_slug = $uri_slug;
+		return TRUE;
+	}
+	
+	public function get_uri_slug()
+	{
+		return $this->_uri_slug;
+	}
+	
+	//Get and SET $creators
+	public function set_creator($creator)
+	{
+		$this->_creators[] = $creator;
+		return TRUE;
+	}
+	
+	public function get_creators()
+	{
+		return $this->_creators;
+	}
+	
+	//Get and SET $subjects
+	public function set_subjects($subjects)
+	{
+		$this->_subjects = $subjects;
+		return TRUE;
+	}
+	
+	public function get_subjects()
+	{
+		return $this->_subjects;
+	}
+	
+	//Get and SET $date
+	public function set_date($date)
+	{
+		$this->_date = $date;
+		return TRUE;
+	}
+	
+	public function get_date()
+	{
+		return $this->_date;
+	}
+	
+	//Get and SET $keywords
+	public function add_keyword($keyword)
+	{
+		$this->_keywords[] = $keyword;
+		return TRUE;
+	}
+	
+	public function get_keywords()
+	{
+		return $this->_keywords;
+	}
+}
