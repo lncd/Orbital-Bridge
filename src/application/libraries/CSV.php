08658acc71041f9d736821eb15c4bacce9ec379d
Harry Newton <h-newton@hotmail.com>
2012-11-20 11:26:58 +0000

    Added CSV library

---
+++ src/application/libraries/CSV.php
@@ -0,0 +1,95 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * CSV Library
+ *
+ * CSV Library.
+ *
+ * @category    Library
+ * @package     Orbital
+ * @author      Harry Newton <hnewton@lincoln.ac.uk>
+ * @link        https://github.com/lncd/Orbital-Bridge
+ */
+
+class CSV {
+
+	/**
+	 * CodeIgniter Instance.
+	 *
+	 * @var $_ci
+	 */
+
+	private $_ci;
+
+	/**
+	 * Constructor
+	 */
+
+	function __construct()
+	{
+		$this->_ci =& get_instance();
+	}
+
+	/**
+	 * Post CURL Request
+	 *
+	 * Sends the CURL request, performing the request sent by another function
+	 *
+	 * string $url     URL to POST CURL request
+	 * array  $fields  Fields send over CURL
+	 *
+	 * @return null
+	 * @access public
+	 */
+
+	public function post_curl_request($url, $fields)
+	{
+		//open connection
+		$ch = curl_init();
+
+		//set the url, number of POST vars, POST data
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, count($fields));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+		//curl_setopt($ch, CURLOPT_USERPWD, 'username' . ":" . 'password');
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: ' . $_SERVER['CKAN_API_KEY']));
+
+		//execute post
+		$result = curl_exec($ch);
+
+		//close connection
+		curl_close($ch);
+
+		return $result;
+	}
+
+	/**
+	 * Get CURL Request
+	 *
+	 * Sends the CURL request, performing the request sent by another function
+	 *
+	 * string $url URL to GET CURL request
+	 *
+	 * @return null
+	 * @access public
+	 */
+
+	public function get_curl_request($url)
+	{
+		//open connection
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: ' . $_SERVER['CKAN_API_KEY']));
+
+		//execute post
+		$result = curl_exec($ch);
+
+		//close connection
+		curl_close($ch);
+
+		return $result;
+	}
+}
