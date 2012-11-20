ce9de932b277f4886a55baf653deb291b75e011c
Harry Newton <h-newton@hotmail.com>
2012-11-20 16:25:21 +0000

    Added CSV library functions back in again

--- src/application/bridge_applications/Csv.php
+++ src/application/bridge_applications/Csv.php
@@ -26,4 +26,60 @@ class Csv {
 		}
 	}';
 
+	/**
+	 * CodeIgniter Instance.
+	 *
+	 * @var $_ci
+	 */
+
+	private $_ci;
+
+	/**
+	 * Constructor
+	 */
+
+	function __construct()
+	{
+		$this->_ci =& get_instance();
+	}
+
+	/**
+	 * Reads CSV and converts to bridge object
+	 *
+	 * string $url URI of CSV to read
+	 *
+	 * @return object $bridge_object The standard bridge object
+	 * @access public
+	 */
+
+	public function read_csv($url)
+	{	
+		$rows = explode("\n", file_get_contents($url));
+		$output = array();
+
+		foreach($rows as $row)
+		{
+			$output[] = explode(",", $row);
+		}		
+		return $output;
+	}
+
+	/**
+	 * Creates CSV from bridge object
+	 *
+	 * string $bridge_object Bridge Object to convert to CSV
+	 *
+	 * @return object $csv CSV output
+	 * @access public
+	 */
+
+	public function convert_tabular_to_text($bridge_object)
+	{	
+		$output = '';
+		foreach($bridge_object as $array_item)
+		{
+			$output .= implode(",", $array_item) . "\n";
+		}
+		return $output;
+	}
 }
