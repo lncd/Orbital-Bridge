1cba7b8ed152185d60d369a0e9304ca175cdd2a9
Nick Jackson <nijackson@lincoln.ac.uk>
2012-11-28 16:19:45 +0000

    Extra tidying of stuff on the Me menu

---
+++ src/application/views/projects/start.php
@@ -0,0 +1,26 @@
+<div class="page-header">
+
+	<h1>
+		<i class="icon-plus"></i> Start a Research Project
+	</h1>
+
+</div>
+
+<div class="row">
+	
+	<div class="span12">
+	
+		<div class="alert alert-block alert-info">
+			<h4>Funded Project?</h4>
+			<p>If you're starting a new funded project, you should instead use the University's <a href="http://ams.lincoln.ac.uk">Award's Management System</a>. Once your project is created using the AMS it's easy to import it back into Orbital.</p>
+		</div>
+		
+		<p class="lead">Step 1: The Basics</p>
+		
+		<p>You can start a new unfunded project from within Orbital, or import information about a funded project you've already created directly from the <a href="http://ams.lincoln.ac.uk">Awards Management System</a>.</p>
+		
+		<p><a href="<?php echo site_url('projects/start'); ?>" class="btn btn-large btn-primary"><i class="icon-arrow-right"></i> Next</a></p>
+		
+	</div>
+			
+</div>
