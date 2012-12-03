fe6a9a14b07b2dde23254397349f5425deb8ff56
Nick Jackson <nijackson@lincoln.ac.uk>
2012-12-03 11:29:35 +0000

    Tidied all category handling to a library, and now able to disable categories.

--- src/application/controllers/wizard.php
+++ src/application/controllers/wizard.php
@@ -4,22 +4,11 @@ class Wizard extends CI_Controller {
 
 	public function view($slug = 'start')
 	{
-		$p_c = new Page_category();
-		$categories = $p_c->get();
-		
-		foreach($categories as $category)
-		{
-			$p = new Page();
-			$p->where_related_page_category_link('page_category_id', $category->id);
-			$p->order_by('order');
-			$pages = $p->get();
-			$category_pages[$category->id] = $pages;
-		}
 		
 		$header = array(
 			'page' => 'wizard',
-			'categories' => $categories,
-			'category_pages' => $category_pages
+			'categories' => $this->bridge->categories(),
+			'category_pages' => $this->bridge->category_pages()
 		);
 		
 		$w = new Wizard_page();
