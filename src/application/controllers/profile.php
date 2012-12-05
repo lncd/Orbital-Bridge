3d7aabd777f45e47b8f3829ea8dab08f2345ea42
Nick Jackson <nijackson@lincoln.ac.uk>
2012-12-05 11:28:56 +0000

    We are a dashboard!

--- src/application/controllers/profile.php
+++ src/application/controllers/profile.php
@@ -22,9 +22,38 @@ class Profile extends CI_Controller {
 			'category_pages' => $this->bridge->category_pages()
 		);
 		
+		$footer['javascript'] = "Morris.Bar({
+			element: 'eprints-output-history',
+			data: [
+				{ y: '2002', a: 1 },
+				{ y: '2008', a: 1 },
+				{ y: '2009', a: 2 },
+				{ y: '2010', a: 3 },
+				{ y: '2011', a: 1 },
+				{ y: '2012', a: 2 }
+			],
+			xkey: 'y',
+			ykeys: ['a'],
+			labels: ['Publications'],
+			hideHover: true
+		});
+		
+		Morris.Donut({
+			element: 'eprints-types',
+			data: [
+				{ label: 'Article', value: 2 },
+				{ label: 'Book Section', value: 4 },
+				{ label: 'Conference or Workshop Item (Other)', value: 2 },
+				{ label: 'Conference or Workshop Item (Paper)', value: 3 },
+				{ label: 'Conference or Workshop Item (Poster)', value: 2 },
+				{ label: 'Teaching Resource', value: 1 },
+				{ label: 'Thesis (Masters)', value: 1 }
+			]
+		});";
+		
 		$this->load->view('inc/head', $header);
 		$this->load->view('profile');
-		$this->load->view('inc/foot');
+		$this->load->view('inc/foot', $footer);
 	}
 }
 
