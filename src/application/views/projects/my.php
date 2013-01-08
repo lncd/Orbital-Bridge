b5db1cf444aac8d34d8aefebaafd3509883869d7
Nick Jackson <nijackson@lincoln.ac.uk>
2013-01-08 11:54:49 +0000

    Stylistic tweaks

--- src/application/views/projects/my.php
+++ src/application/views/projects/my.php
@@ -34,46 +34,70 @@
 
 </div>
 
+<?php if (count($active) > 0): ?>
+
 <div id="projectsTimeline" class="gantt"></div>
 
-<div class="row">
+<?php endif; ?>
+		
+<p><a class="btn btn-success" href="#"><i class="icon-plus"></i> Add New Project</a></p>
+
+<?php if (count($active) > 0): ?>
+
+<h2>Current Projects</h2>
+
+<table class="table table-bordered table-striped table-condensed">
+	<thead>
+		<tr>
+			<th>Project Name</th>
+		</tr>
+	</thead>
+	<tbody>
 	
-	<div class="span12">
-		
-		<p><a class="btn btn-success" href="#"><i class="icon-plus"></i> Add</a></p>
-		
-		<h2>Current Projects</h2>
-		
-		<table class="table table-bordered table-striped">
-			<tbody>
-			
-			<?php
-				foreach($active as $project)
-				{
-					echo '<tr>';
-					echo '<td><a href = ' . site_url('projects/' . $project->id) . '>' . $project->title . '</a></td>';
-					echo '</tr>';
-				}
-			?>
-			
-			</tbody>
-		</table>
-		
-		<h2>Past Projects</h2>
-		<button id="inactive_button" class="icon-cog"> Show / Hide</button>
-		
-		<table id="inactive" class="table table-bordered table-striped" style="display:none">
-			<tbody>
-				<?php
-					foreach($inactive as $project)
-					{
-						echo '<tr>';
-						echo '<td><a href = ' . site_url('projects/' . $project->id) . '>' . $project->title . '</a></td>';
-						echo '</tr>';
-					}
-				?>
-			</tbody>
-		</table>
-	</div>
-			
+	<?php
+		foreach($active as $project)
+		{
+			echo '<tr>';
+			echo '<td><a href = ' . site_url('projects/' . $project->id) . '>' . $project->title . '</a></td>';
+			echo '</tr>';
+		}
+	?>
+	
+	</tbody>
+</table>
+
+<?php
+
+endif;
+
+if (count($inactive) > 0):
+
+?>
+
+<h2>Past Projects</h2>
+
+<p><a id="inactive_button"><i class="icon-double-angle-down"></i> Show / Hide</a></p>
+
+<div id="inactive" style="display:none">
+
+<table class="table table-bordered table-striped table-condensed">
+	<thead>
+		<tr>
+			<th>Project Name</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+			foreach($inactive as $project)
+			{
+				echo '<tr>';
+				echo '<td><a href = ' . site_url('projects/' . $project->id) . '>' . $project->title . '</a></td>';
+				echo '</tr>';
+			}
+		?>
+	</tbody>
+</table>
+
 </div>
+
+<?php endif; ?>
