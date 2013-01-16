9af1a4cc5d5c1ba1e8a61c6bcac488a363594d11
Harry Newton <h-newton@hotmail.com>
2013-01-16 13:17:48 +0000

    Added the unauthorised view, so the previous commit will actually work

---
+++ src/application/views/projects/unauthorised.php
@@ -0,0 +1,16 @@
+<div class="page-header">
+
+	<h1>
+		Unauthorised
+	</h1>
+
+</div>
+<div class="row">
+	
+	<div class="span12">
+		
+		You are not authorised to view this project. It is not one of yours!
+	
+	</div>
+	
+</div>
