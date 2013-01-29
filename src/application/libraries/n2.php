fd7d81e0eaa5aae2b484a0bb042d8f81d338d991
Nick Jackson <nijackson@lincoln.ac.uk>
2013-01-29 15:25:41 +0000

    Amazement!

--- src/application/libraries/n2.php
+++ src/application/libraries/n2.php
@@ -11,122 +11,29 @@
  * @link        https://github.com/lncd/Orbital-Bridge
  */
 
-class N2 {
-	
-	/**
-	 * Get CURL Request
-	 *
-	 * Get CURL request
-	 *
-	 * @return null
-	 * @access public
-	 */
+class N2 
+{
 
-	public function get_curl_request($url, $access_token = NULL)
+	private $client;
+
+	public function __construct()
 	{
-		//open connection
-		$ch = curl_init();
-		
-		$headers = array(
-		);
-		if ($access_token !== NULL)
-		{
-			$headers[] = 'Authorization: Bearer ' . base64_encode($access_token);
-		}
+		$this->client = new Nucleus\Client(array(
+	    	'access_token' => '123456'
+	    ));
+	}
 
-		//set the url, number of POST vars, POST data
-		curl_setopt($ch, CURLOPT_URL, $_SERVER['NUCLEUS_BASE_URI'] . $_SERVER['NUCLEUS_BASE_URI'] . $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	public function __call($method, $params)
+    {
+    
+    	if (isset($params[0]))
+    	{
+	    	return $this->client->$method($params[0]);
+	    }
+	    else
+	    {
+		    return $this->client->$method();
+	    }
+    }
 
-		//execute post
-		$result = curl_exec($ch);
-
-		//close connection
-		curl_close($ch);
-
-		return $result;
-	}	
-	
-	
-	/**
-	 * Post CURL Request
-	 *
-	 * Sends the CURL request, performing the request sent by another function
-	 *
-	 * string $url     URL to POST CURL request
-	 * array  $fields  Fields send over CURL
-	 *
-	 * @return null
-	 * @access public
-	 */
-
-	public function post_curl_request($url, $fields, $access_token = NULL)
-	{
-		//open connection
-		$ch = curl_init();
-		
-		$headers = array(
-			'Content-Type: application/json'
-		);
-		if ($access_token !== NULL)
-		{
-			$headers[] = 'Authorization: Bearer ' . base64_encode($access_token);
-		}
-
-		//set the url, number of POST vars, POST data
-		curl_setopt($ch, CURLOPT_URL, $_SERVER['NUCLEUS_BASE_URI'] . $url);
-		curl_setopt($ch, CURLOPT_POST, count($fields));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-		//curl_setopt($ch, CURLOPT_USERPWD, 'username' . ":" . 'password');
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-		//execute post
-		$result = curl_exec($ch);
-
-		//close connection
-		curl_close($ch);
-
-		return $result;
-	}	
-
-	/**
-	 * Delete CURL Request
-	 *
-	 * Sends the CURL request, performing the request sent by another function
-	 *
-	 * string $url     URL
-	 * array  $fields  Fields send over CURL
-	 *
-	 * @return null
-	 * @access public
-	 */
-
-	public function delete_curl_request($url, $access_token = NULL)
-	{		
-		$headers = array();
-		if ($access_token !== NULL)
-		{
-			$headers[] = 'Authorization: Bearer ' . base64_encode($access_token);
-		}
-
-		//open connection
-		$ch = curl_init();
-
-		//set the url, number of POST vars, POST data
-		curl_setopt($ch, CURLOPT_URL, $_SERVER['NUCLEUS_BASE_URI'] . $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-		//execute post
-		$result = curl_exec($ch);
-
-		//close connection
-		curl_close($ch);
-
-		return $result;
-	}	
 }
