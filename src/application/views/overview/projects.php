8cd3b359e9898c56dd100c3005848d2be6715673
Nick Jackson <nijackson@lincoln.ac.uk>
2013-02-11 14:27:48 +0000

    Research overview page.

---
+++ src/application/views/overview/projects.php
@@ -0,0 +1,49 @@
+<div class="page-header">
+
+	<h1>
+		<i class="icon-dashboard"></i> Research Projects Overview
+	</h1>
+
+</div>
+
+<div class="row">
+
+	<div class="span12">
+	
+		<h4>Total Research Projects</h4>
+	
+		<p class="bignumber"><?php echo number_format($projects_total); ?></p>
+	
+	</div>
+
+</div>
+
+<hr>
+
+<div class="row">
+	
+	<div class="span4">
+		
+		<h4>Project Types</h4>
+		
+		<div id="funded" style="width:100%; height:300px;"></div>
+		
+	</div>
+	
+	<div class="span4">
+		
+		<h4>Success of Funded Projects</h4>
+		
+		<div id="funding-success" style="width:100%; height:300px;"></div>
+		
+	</div>
+	
+	<div class="span4">
+		
+		<h4>Funding Types of Successful Projects</h4>
+		
+		<div id="funding-types" style="width:100%; height:300px;"></div>
+		
+	</div>
+
+</div>
